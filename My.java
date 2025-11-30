3b042fb47e8781540365834ed3f1d5ceaf0ba6b0
Alok Raj <alokraj32135@gmail.com>
2025-11-30 23:05:09 +0530

    Update My.java

--- My.java
+++ My.java
@@ -109,6 +109,4 @@ class My {
     {boolean[]p=new boolean[n+1];for(int i=2;i*i<=n;i++)
         if(!p[i])for(int j=i*i;j<=n;j+=i)p[j]=true;for(int i=2;i<=n;i++)if(!p[i])System.out.print(i+" ");} 
 
-// }
-    // 
 }
